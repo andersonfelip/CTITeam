67b43573be18b4c6bf4d59c065c9f2b0a54a17cd
Anderson Felipe <anderson_felipe@hotmail.com>
2019-01-19 16:28:32 -0300

    automerge

---
+++ app/src/main/java/com/example/nadjane/cti/cadastroActivity.java
@@ -0,0 +1,13 @@
+package com.example.nadjane.cti;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
